a608cd05a669b182c2602da80322ba617068d0f2
hecnavar <hmnm0007@gmail.com>
2025-05-02 17:31:12 -0600

    feat: Add basic routing and navigation menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/auth/Login';
+import Signup from './components/auth/Signup';
+import DashboardPage from './pages/DashboardPage';
+import { Link } from 'react-router-dom';
 
-const App = () => {
+function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<div>Bienvenido</div>} />
-      </Routes>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
-};
+}
 
 export default App;
