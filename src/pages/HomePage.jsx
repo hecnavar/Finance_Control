c98bb1a7ea8bec800e4373390c6397d3973c8477
hecnavar <hmnm0007@gmail.com>
2025-05-02 23:02:55 -0600

    Feat: New import added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,12 +3,14 @@ import CreditCardList from '../components/CreditCardList';
 import BudgetOverview from '../components/BudgetOverview';
 import ExpenseList from '../components/ExpenseList';
 import AddCreditCardForm from '../components/AddCreditCardForm';
+import BudgetSetupForm from '../components/BudgetSetupForm';
 
-function HomePage({ addCreditCard, creditCards }) {
+function HomePage({ addCreditCard, creditCards, budget, onBudgetUpdate }) {
   return (
     <div>
       <h1>Panel Principal</h1>
-      <BudgetOverview />
+      <BudgetSetupForm onBudgetUpdate={onBudgetUpdate} />
+      <BudgetOverview budget={budget} />
       <AddCreditCardForm onAddCreditCard={addCreditCard} />
       <CreditCardList creditCards={creditCards} />
       <ExpenseList />
