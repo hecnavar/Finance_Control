73b94f0022a50d40df4a689ef9f6e25a0f34cea7
hecnavar <hmnm0007@gmail.com>
2025-05-02 22:28:05 -0600

    Feat: New useState added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-// src/App.js
-import React from 'react';
+import React, { useState } from 'react';
 import HomePage from './pages/HomePage';
 
 function App() {
+  const [creditCards, setCreditCards] = useState([]);
+
+  const addCreditCard = (newCard) => {
+    setCreditCards([...creditCards, newCard]);
+  };
+
   return (
     <div>
-      <HomePage />
+      <HomePage addCreditCard={addCreditCard} creditCards={creditCards} />
     </div>
   );
 }
