8b86dda32930edbbe35eb98059a45a007bf77ec2
hecnavar <hmnm0007@gmail.com>
2025-05-02 22:23:28 -0600

    Feat: New component AddCreditCardForm added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import CreditCardList from '../components/CreditCardList';
 import BudgetOverview from '../components/BudgetOverview';
 import ExpenseList from '../components/ExpenseList';
+import AddCreditCardForm from '../components/AddCreditCardForm';
 
-function HomePage() {
+function HomePage({ addCreditCard, creditCards }) {
   return (
     <div>
       <h1>Panel Principal</h1>
       <BudgetOverview />
-      <CreditCardList />
+      <AddCreditCardForm onAddCreditCard={addCreditCard} />
+      <CreditCardList creditCards={creditCards} />
       <ExpenseList />
     </div>
   );
