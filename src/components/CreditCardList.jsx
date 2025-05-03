dda138088795699681377ec6a8e56920abbbe719
hecnavar <hmnm0007@gmail.com>
2025-05-02 22:24:31 -0600

    Feat: Creat a logic for show total numer of cards

--- src/components/CreditCardList.jsx
+++ src/components/CreditCardList.jsx
@@ -1,10 +1,22 @@
+import React from 'react';
 
-function CreditCardList() {
-    return (
-      <div>
-        <h2>Mis Tarjetas de Crédito</h2>
-      </div>
-    );
-  }
-  
-  export default CreditCardList;
+function CreditCardList({ creditCards }) {
+  return (
+    <div>
+      <h2>Mis Tarjetas de Crédito</h2>
+      {creditCards.length === 0 ? (
+        <p>No has agregado ninguna tarjeta de crédito aún.</p>
+      ) : (
+        <ul>
+          {creditCards.map((card, index) => (
+            <li key={index}>
+              {card.bankName} - Últimos dígitos: {card.cardNumber.slice(-4)} - Límite: ${card.creditLimit}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default CreditCardList;
