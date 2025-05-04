0fd95852a851ed5cdbe55e3f0f0fe6f8a346e8cd
hecnavar <hmnm0007@gmail.com>
2025-05-03 21:06:06 -0600

    Feat: Update the ExpenseList file to receive the list of expenses as a prop and render them

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -1,9 +1,23 @@
-function ExpenseList() {
-    return (
-      <div>
-        <h2>Últimos Gastos</h2>
-      </div>
-    );
-  }
-  
-  export default ExpenseList;
+import React from 'react';
+
+function ExpenseList({ expenses }) {
+  return (
+    <div>
+      <h2>Últimos Gastos</h2>
+      {expenses.length === 0 ? (
+        <p>No has registrado ningún gasto aún.</p>
+      ) : (
+        <ul>
+          {expenses.map((expense, index) => (
+            <li key={index}>
+              {expense.description} - ${expense.amount} - {expense.date} ({expense.category})
+              {expense.creditCardId && ` - Tarjeta: ...${expense.creditCardId.slice(-4)}`}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default ExpenseList;
