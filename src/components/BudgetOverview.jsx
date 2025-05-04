5ea74f4cefdfa772ad89094f9477cb240f3e5122
hecnavar <hmnm0007@gmail.com>
2025-05-04 00:40:06 -0600

    Feat: Update to show information related with total cost

--- src/components/BudgetOverview.jsx
+++ src/components/BudgetOverview.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-function BudgetOverview({ budget }) {
+function BudgetOverview({ budget, totalExpenses }) {
+  const remainingBudget = budget.total - totalExpenses;
+
   return (
     <div>
       <h2>Resumen del Presupuesto</h2>
       <p>Presupuesto Total Mensual: ${budget.total}</p>
+      <p>Total Gastado: ${totalExpenses}</p>
+      <p>Presupuesto Restante: ${remainingBudget >= 0 ? `$${remainingBudget}` : `-$${Math.abs(remainingBudget)}`}</p>
     </div>
   );
 }
