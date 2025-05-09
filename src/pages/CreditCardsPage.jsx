d5876b822cbfeb54a9e7b340a4c6dd3aa1840ffd
hecnavar <hmnm0007@gmail.com>
2025-05-08 22:35:01 -0600

    Feature: Implement basic responsive styles on the Home Page and CreditCards Page. Basic responsive styles were applied using CSS Modules and Media Queries to improve adaptability to different screen sizes on the main home and credit card management pages.

--- src/pages/CreditCardsPage.jsx
+++ src/pages/CreditCardsPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import CreditCardList from '../components/CreditCardList';
 import AddCreditCardForm from '../components/AddCreditCardForm';
+import styles from './CreditCardsPage.module.css';
 
 function CreditCardsPage({ onAddCreditCard, creditCards }) {
   return (
-    <div>
+    <div className={styles.cardsContainer}>
       <h2>Gestión de Tarjetas de Crédito</h2>
       <AddCreditCardForm onAddCreditCard={onAddCreditCard} />
       <CreditCardList creditCards={creditCards} />
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import BudgetOverview from '../components/BudgetOverview';
+import styles from './HomePage.module.css';
 
 function HomePage({ budget, totalExpenses, estimatedFixedExpenses }) {
   return (
-    <div>
+    <div className={styles.main_container}>
       <h1>Panel Principal</h1>
       <BudgetOverview budget={budget} totalExpenses={totalExpenses} estimatedFixedExpenses={estimatedFixedExpenses} />
     </div>
